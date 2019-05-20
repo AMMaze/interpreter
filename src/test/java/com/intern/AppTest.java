6852d064c254a29f614eb554209ae869f2f15409
Marat Abbas Mazen <unlock96@gmail.com>
2019-05-21 00:25:24 +0300

    Tests

--- src/test/java/com/intern/AppTest.java
+++ src/test/java/com/intern/AppTest.java
@@ -2,7 +2,10 @@ package com.intern;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.*;
 
 /**
  * Unit test for simple App.
@@ -16,5 +19,157 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testBinary() {
+        InputStream stdin = System.in;
+        PrintStream stdout = System.out;
+        try {
+            System.setIn(new ByteArrayInputStream("(1+1)".getBytes()));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(baos);
+            System.setOut(ps);
+            App.main(null);
+            Assert.assertEquals("Result: 2", baos.toString().trim());
+        } finally {
+            System.setIn(stdin);
+            System.setOut(stdout);
+        }
+
+        try {
+            System.setIn(new ByteArrayInputStream("(1-1)".getBytes()));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(baos);
+            System.setOut(ps);
+            App.main(null);
+            Assert.assertEquals("Result: 0", baos.toString().trim());
+        } finally {
+            System.setIn(stdin);
+            System.setOut(stdout);
+        }
+
+        try {
+            System.setIn(new ByteArrayInputStream("(1*2)".getBytes()));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(baos);
+            System.setOut(ps);
+            App.main(null);
+            Assert.assertEquals("Result: 2", baos.toString().trim());
+        } finally {
+            System.setIn(stdin);
+            System.setOut(stdout);
+        }
+
+        try {
+            System.setIn(new ByteArrayInputStream("(2/2)".getBytes()));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(baos);
+            System.setOut(ps);
+            App.main(null);
+            Assert.assertEquals("Result: 1", baos.toString().trim());
+        } finally {
+            System.setIn(stdin);
+            System.setOut(stdout);
+        }
+
+        try {
+            System.setIn(new ByteArrayInputStream("(1%2)".getBytes()));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(baos);
+            System.setOut(ps);
+            App.main(null);
+            Assert.assertEquals("Result: 1", baos.toString().trim());
+        } finally {
+            System.setIn(stdin);
+            System.setOut(stdout);
+        }
+
+        try {
+            System.setIn(new ByteArrayInputStream("(1>1)".getBytes()));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(baos);
+            System.setOut(ps);
+            App.main(null);
+            Assert.assertEquals("Result: 0", baos.toString().trim());
+        } finally {
+            System.setIn(stdin);
+            System.setOut(stdout);
+        }
+
+        try {
+            System.setIn(new ByteArrayInputStream("(1<2)".getBytes()));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(baos);
+            System.setOut(ps);
+            App.main(null);
+            Assert.assertEquals("Result: 1", baos.toString().trim());
+        } finally {
+            System.setIn(stdin);
+            System.setOut(stdout);
+        }
+
+        try {
+            System.setIn(new ByteArrayInputStream("(1=1)".getBytes()));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(baos);
+            System.setOut(ps);
+            App.main(null);
+            Assert.assertEquals("Result: 1", baos.toString().trim());
+        } finally {
+            System.setIn(stdin);
+            System.setOut(stdout);
+        }
+    }
+
+    @Test
+    public void testUnary() {
+        InputStream stdin = System.in;
+        PrintStream stdout = System.out;
+        try {
+            System.setIn(new ByteArrayInputStream("(-1)".getBytes()));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(baos);
+            System.setOut(ps);
+            App.main(null);
+            Assert.assertEquals("Result: -1", baos.toString().trim());
+        } finally {
+            System.setIn(stdin);
+            System.setOut(stdout);
+        }
+    }
+
+    @Test
+    public void testBraces() {
+        InputStream stdin = System.in;
+        PrintStream stdout = System.out;
+        try {
+            System.setIn(new ByteArrayInputStream("((((1))))".getBytes()));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(baos);
+            System.setOut(ps);
+            App.main(null);
+            Assert.assertEquals("Result: 1", baos.toString().trim());
+        } finally {
+            System.setIn(stdin);
+            System.setOut(stdout);
+        }
+    }
+
+    @Test
+    public void testExpression() {
+        InputStream stdin = System.in;
+        PrintStream stdout = System.out;
+        try {
+            System.setIn(new ByteArrayInputStream("(1-((3+2)*3))".getBytes()));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(baos);
+            System.setOut(ps);
+            App.main(null);
+            Assert.assertEquals("Result: -14", baos.toString().trim());
+        } finally {
+            System.setIn(stdin);
+            System.setOut(stdout);
+        }
     }
 }
